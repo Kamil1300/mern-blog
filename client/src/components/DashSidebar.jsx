bde88bb30cf61c356c221168e5af7d5eb803020f
Shaikh Kamil <108180928+Kamil1300@users.noreply.github.com>
2024-07-23 16:00:59 +0530

    Adding Dash Comment

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Sidebar } from 'flowbite-react'
-import { HiArrowSmRight, HiUser, HiDocumentText, HiOutlineUserGroup } from 'react-icons/hi'
+import { HiArrowSmRight, HiUser, HiDocumentText, HiOutlineUserGroup, HiAnnotation } from 'react-icons/hi'
 import { useLocation, Link } from 'react-router-dom'
 import { signoutSuccess } from '../redux/users/userSlice'
 import { useDispatch, useSelector } from 'react-redux'
@@ -55,6 +55,7 @@ export default function DashSidebar() {
                         </Link>
                     )}
                     {currentUser.isAdmin && (
+                        <>
                         <Link to='/dashboard?tab=users'>
                             <Sidebar.Item
                                 active={tab === 'users'}
@@ -64,6 +65,16 @@ export default function DashSidebar() {
                                 Users
                             </Sidebar.Item>
                         </Link>
+                        <Link to='/dashboard?tab=comments'>
+                            <Sidebar.Item
+                                active={tab === 'comments'}
+                                icon={HiAnnotation}
+                                as='div'
+                            >
+                                Comments
+                            </Sidebar.Item>
+                        </Link>
+                        </>
                     )}
 
                     <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignout}>Sign Out</Sidebar.Item>
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,6 +4,8 @@ import DashSidebar from '../components/DashSidebar'
 import DashProfile from '../components/DashProfile'
 import DashPosts from '../components/DashPosts'
 import DashUsers from '../components/DashUsers'
+import DashComments from '../components/DashComments'
+import DashboardComp from '../components/DashboardComp'
 
 export default function Dashboard() {
 
@@ -30,6 +32,10 @@ export default function Dashboard() {
       {tab === 'posts' && <DashPosts/>}
       {/* Users  */}
       {tab === 'users' && <DashUsers/>}
+      {/* Comments */}
+      {tab === 'comments' && <DashComments/>}
+      {/* dashboard comp  */}
+      {tab === 'dash' && <DashboardComp/>}
     </div>
   )
 }
