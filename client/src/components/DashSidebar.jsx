e05aecbb1d49e28a9cb48205d92b725c94aceec7
Shaikh Kamil <108180928+Kamil1300@users.noreply.github.com>
2024-07-25 16:14:16 +0530

    add dashboard overview to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Sidebar } from 'flowbite-react'
-import { HiArrowSmRight, HiUser, HiDocumentText, HiOutlineUserGroup, HiAnnotation } from 'react-icons/hi'
+import { HiArrowSmRight, HiUser, HiDocumentText, HiOutlineUserGroup, HiAnnotation, HiChartPie } from 'react-icons/hi'
 import { useLocation, Link } from 'react-router-dom'
 import { signoutSuccess } from '../redux/users/userSlice'
 import { useDispatch, useSelector } from 'react-redux'
@@ -8,7 +8,7 @@ import { useDispatch, useSelector } from 'react-redux'
 export default function DashSidebar() {
     const location = useLocation()
     const dispatch = useDispatch()
-    const {currentUser} = useSelector((state) => state.user)
+    const { currentUser } = useSelector((state) => state.user)
     const [tab, setTab] = useState('')
 
     useEffect(() => {
@@ -39,8 +39,15 @@ export default function DashSidebar() {
         <Sidebar className='w-full md:w-56'>
             <Sidebar.Items>
                 <Sidebar.ItemGroup className='flex flex-col gap-1'>
+                    {
+                        currentUser && currentUser.isAdmin && (
+                            <Link to='/dashboard?tab=dash'>
+                                <Sidebar.Item active={tab === 'dash' || !tab} icon={HiChartPie} as='div'>Dashboard</Sidebar.Item>
+                            </Link>
+                        )
+                    }
                     <Link to='/dashboard?tab=profile'>
-                        <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={currentUser.isAdmin ? 'Admin' : 'User'} labelColor='dark' as='div'>Profile</Sidebar.Item>
+                        <Sidebar.Item icon={HiUser} label={currentUser.isAdmin ? 'Admin' : 'User'} labelColor='dark' as='div'>Profile</Sidebar.Item>
                     </Link>
 
                     {currentUser.isAdmin && (
@@ -56,24 +63,24 @@ export default function DashSidebar() {
                     )}
                     {currentUser.isAdmin && (
                         <>
-                        <Link to='/dashboard?tab=users'>
-                            <Sidebar.Item
-                                active={tab === 'users'}
-                                icon={HiOutlineUserGroup}
-                                as='div'
-                            >
-                                Users
-                            </Sidebar.Item>
-                        </Link>
-                        <Link to='/dashboard?tab=comments'>
-                            <Sidebar.Item
-                                active={tab === 'comments'}
-                                icon={HiAnnotation}
-                                as='div'
-                            >
-                                Comments
-                            </Sidebar.Item>
-                        </Link>
+                            <Link to='/dashboard?tab=users'>
+                                <Sidebar.Item
+                                    active={tab === 'users'}
+                                    icon={HiOutlineUserGroup}
+                                    as='div'
+                                >
+                                    Users
+                                </Sidebar.Item>
+                            </Link>
+                            <Link to='/dashboard?tab=comments'>
+                                <Sidebar.Item
+                                    active={tab === 'comments'}
+                                    icon={HiAnnotation}
+                                    as='div'
+                                >
+                                    Comments
+                                </Sidebar.Item>
+                            </Link>
                         </>
                     )}
 
